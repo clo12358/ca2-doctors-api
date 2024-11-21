a1d0945b6fd4803deab9d9fae02e6ad5441eacaa
chloe 3 <125874461+clo12358@users.noreply.github.com>
2024-11-21 17:24:32 +0000

    Connected API to doctors/patients

--- src/pages/doctors/Index.js
+++ src/pages/doctors/Index.js
@@ -1,33 +1,54 @@
-const Index = () => {
-    return(
-        <>
-        <container>
-            <div className="overflow-x-auto">
-            <table className="table">
-                {/* head */}
-                <thead>
-                <tr> 
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Email</th>
-                    <th>Phone No.</th>
-                    <th>Specialisation</th>
-                </tr>
-                </thead>
-                <tbody>
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-                <tr className="hover">
-                    <td>Aoife</td>
-                    <td>Lynch</td>
-                    <td>alynch@gmail.com</td>
-                    <td>0873452765</td>
-                    <td>Podiatrist</td>
-                </tr>
-                </tbody>
-            </table>
-            </div>
-        </container>
-        </>       
+const Index = () => {
+    const [doctors, setDoctors] = useState(null);
+
+    useEffect(() => {
+        axios.get('https://fed-medical-clinic-api.vercel.app/doctors')
+            .then(response => {
+                console.log(response.data);
+                setDoctors(response.data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, []);
+
+    if (!doctors) return 'Loading...';
+
+    return (
+        <>
+            <container>
+                <div className="overflow-x-auto">
+                    <table className="table">
+                        {/* Head section */}
+                        <thead>
+                            <tr>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Email</th>
+                                <th>Phone No.</th>
+                                <th>Specialisation</th>
+                            </tr>
+                        </thead>
+
+                        {/* Body section */}
+                        <tbody>
+                            {doctors.map(({ _id, first_name, last_name, email, phone, specialisation }) => (
+                                <tr key={_id} className="hover">
+                                    <td>{first_name}</td>
+                                    <td>{last_name}</td>
+                                    <td>{email}</td>
+                                    <td>{phone}</td>
+                                    <td>{specialisation}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </container>
+        </>
     );
 };
 
--- src/pages/patients/Index.js
+++ src/pages/patients/Index.js
@@ -1,35 +1,56 @@
-const Index = () => {
-    return(
-        <>
-        <container>
-            <div className="overflow-x-auto">
-            <table className="table">
-                {/* head */}
-                <thead>
-                <tr> 
-                    <th>Date</th>
-                    <th>Doctor</th>
-                    <th>Patient</th>
-                    <th>Phone No.</th>
-                    <th>Date of birth</th>
-                    <th>Address</th>
-                </tr>
-                </thead>
-                <tbody>
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-                <tr className="hover">
-                    <td>Julia</td>
-                    <td>Szew</td>
-                    <td>jszew@gmail.com</td>
-                    <td>0874562453</td>
-                    <td>01/01/2001</td>
-                    <td>Balbriggan</td>
-                </tr>
-                </tbody>
-            </table>
-            </div>
-        </container>
-        </>       
+const Index = () => {
+    const [patients, setPatients] = useState(null);
+
+    useEffect(() => {
+        axios.get('https://fed-medical-clinic-api.vercel.app/patients')
+            .then(response => {
+                console.log(response.data);
+                setPatients(response.data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, []);
+
+    if (!patients) return 'Loading...';
+
+    return (
+        <>
+            <container>
+                <div className="overflow-x-auto">
+                    <table className="table">
+                        {/* Head section */}
+                        <thead>
+                            <tr>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Email</th>
+                                <th>Phone No.</th>
+                                <th>Date of birth</th>
+                                <th>Address</th>
+                            </tr>
+                        </thead>
+
+                        {/* Body section */}
+                        <tbody>
+                            {patients.map(({ _id, first_name, last_name, email, phone, date_of_birth, address }) => (
+                                <tr key={_id} className="hover">
+                                    <td>{first_name}</td>
+                                    <td>{last_name}</td>
+                                    <td>{email}</td>
+                                    <td>{phone}</td>
+                                    <td>{date_of_birth}</td>
+                                    <td>{address}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </container>
+        </>
     );
 };
 
